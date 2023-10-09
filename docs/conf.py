24d4480dab07821bd768161104870bd840aed89e
gatienduboc <gatien.duboc@gmail.com>
2023-10-09 21:41:32 +0200

    test déploiement

--- docs/conf.py
+++ docs/conf.py
@@ -53,10 +53,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# Change 09/10/2023 import sphinx_rtd_theme
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#Modif 09/10/2023 html_static_path = ['_static']
